0b46b3c52fc5fa9fab121b02fbc462943685a391
Devasheesh Mishra <devasheesh@Devasheeshs-MacBook-Air.local>
2024-07-29 02:07:07 +0530

    some more failed tries hi2hinglish

---
+++ devanagari_chars_sounds.py
@@ -0,0 +1,104 @@
+hindi_to_english = {
+    # Vowels (स्वर)
+    'अ': 'a',   # Short 'a' sound
+    'आ': 'aa',  # Long 'a' sound
+    'इ': 'i',   # Short 'i' sound
+    'ई': 'ee',  # Long 'i' sound
+    'उ': 'u',   # Short 'u' sound
+    'ऊ': 'oo',  # Long 'u' sound
+    'ए': 'e',   # 'e' sound as in 'bed'
+    'ऐ': 'ai',  # 'ai' sound as in 'hair'
+    'ओ': 'o',   # 'o' sound as in 'go'
+    'औ': 'au',  # 'au' sound as in 'cow'
+
+    # Consonants (व्यंजन)
+    # Velar consonants
+    'क': 'k',   # Unaspirated 'k'
+    'ख': 'kh',  # Aspirated 'kh'
+    'ग': 'g',   # Unaspirated 'g'
+    'घ': 'gh',  # Aspirated 'gh'
+    'ङ': 'ng',  # Nasal 'ng' sound
+
+    # Palatal consonants
+    'च': 'ch',  # Unaspirated 'ch'
+    'छ': 'chh', # Aspirated 'chh'
+    'ज': 'j',   # Unaspirated 'j'
+    'झ': 'jh',  # Aspirated 'jh'
+    'ञ': 'n',   # Nasal 'n' sound (palatal)
+
+    # Retroflex consonants
+    'ट': 't',   # Unaspirated 't' (retroflex)
+    'ठ': 'th',  # Aspirated 'th' (retroflex)
+    'ड': 'd',   # Unaspirated 'd' (retroflex)
+    'ढ': 'dh',  # Aspirated 'dh' (retroflex)
+    'ण': 'n',   # Nasal 'n' sound (retroflex)
+
+    # Dental consonants
+    'त': 't',   # Unaspirated 't' (dental)
+    'थ': 'th',  # Aspirated 'th' (dental)
+    'द': 'd',   # Unaspirated 'd' (dental)
+    'ध': 'dh',  # Aspirated 'dh' (dental)
+    'न': 'n',   # Nasal 'n' sound (dental)
+
+    # Labial consonants
+    'प': 'p',   # Unaspirated 'p'
+    'फ': 'ph',  # Aspirated 'ph'
+    'ब': 'b',   # Unaspirated 'b'
+    'भ': 'bh',  # Aspirated 'bh'
+    'म': 'm',   # Nasal 'm' sound
+
+    # Semivowels and liquids
+    'य': 'y',   # 'y' sound
+    'र': 'r',   # 'r' sound
+    'ल': 'l',   # 'l' sound
+    'व': 'v',   # 'v' sound
+
+    # Sibilants and 'h' sound
+    'श': 'sh',  # 'sh' sound (palatal)
+    'ष': 'sh',  # 'sh' sound (retroflex)
+    'स': 's',   # 's' sound
+    'ह': 'h',   # 'h' sound
+
+    # Conjunct consonants
+    'क्ष': 'ksh', # 'ksh' sound (क् + ष)
+    'त्र': 'tr',  # 'tr' sound (त् + र)
+    'ज्ञ': 'gy',  # 'gy' sound (ज् + ञ)
+
+    # Flapped consonants
+    'ड़': 'r',   # Flapped 'r' sound
+    'ढ़': 'rh',  # Aspirated flapped 'r' sound
+
+    # Vowel diacritics (मात्राएँ)
+    'ा': 'a',   # Long 'a' sound diacritic
+    'ि': 'i',   # Short 'i' sound diacritic
+    'ी': 'ee',  # Long 'i' sound diacritic
+    'ु': 'u',   # Short 'u' sound diacritic
+    'ू': 'oo',  # Long 'u' sound diacritic
+    'े': 'e',   # 'e' sound diacritic
+    'ै': 'ai',  # 'ai' sound diacritic
+    'ो': 'o',   # 'o' sound diacritic
+    'ौ': 'au',  # 'au' sound diacritic
+
+    # Other diacritics and symbols
+    'ं': 'n',   # Anusvara (nasal sound)
+    'ः': 'h',   # Visarga (voiceless glottal fricative)
+    '्': '',    # Virama (vowel omission sign)
+
+    # Punctuation and numbers
+    '।': '.',   # Danda (full stop)
+    '॥': '..',  # Double Danda (double full stop)
+    '०': '0', '१': '1', '२': '2', '३': '3', '४': '4',  # Hindi numerals
+    '५': '5', '६': '6', '७': '7', '८': '8', '९': '9',  # Hindi numerals
+
+    # Additional characters
+    'ृ': 'ri',  # 'ri' sound diacritic
+    'ऽ': '\'',   # Avagraha (apostrophe-like symbol)
+    'ँ': 'n',   # Chandrabindu (nasalization)
+    '॑': '',    # Vedic accent (udatta) - no direct Hinglish equivalent
+    '॒': '',    # Vedic accent (anudatta) - no direct Hinglish equivalent
+    '़': '',     # Nukta (modifies consonant sound) - no direct Hinglish equivalent
+    'ऋ': 'ri',  # Vocalic 'r' sound
+    'ॉ': 'o',   # Candra O (short 'o' sound)
+    'ॐ': 'om',  # Om symbol
+    'ऑ': 'o'    # Candra A (short 'o' sound)
+}
